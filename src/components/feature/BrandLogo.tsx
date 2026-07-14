6ededf91a8ef3aa1aea1022b5c34453125487a2c
William DeBruine <debruine-consulting@protonmail.com>
2026-07-13 20:41:04 -0500

    checkpoint before checking out customer-style-updates

--- src/components/feature/BrandLogo.tsx
+++ src/components/feature/BrandLogo.tsx
@@ -8,16 +8,16 @@ interface BrandLogoProps {
 const logoSrc = `${__BASE_PATH__}logo.png`;
 
 export default function BrandLogo({
-  showText = true,
+  showText = false,
   className = "",
   textClassName = "font-heading font-bold text-lg md:text-xl tracking-tight whitespace-nowrap",
-  imageClassName = "h-[60px] w-[60px] md:h-[66px] md:w-[66px] object-contain shrink-0",
+  imageClassName = "h-14 w-14 md:h-16 md:w-16 object-contain shrink-0",
 }: BrandLogoProps) {
   return (
     <span className={`flex items-center gap-2.5 min-w-0 ${className}`}>
       <img
         src={logoSrc}
-        alt="All Kinds of Cleaning logo"
+        alt="All Kinds of Cleaning"
         className={imageClassName}
       />
       {showText && <span className={textClassName}>All Kinds of Cleaning</span>}
